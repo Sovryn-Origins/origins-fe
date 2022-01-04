b0b91df7e67d2f0be31fa2499b79466ad530f420
Bi Wu <alerk_star@outlook.com>
2022-01-04 23:14:27 +0800

    BuyPage/Tabbar: styled components -> module css;

--- src/app/pages/BuyPage/components/Tabbar/index.tsx
+++ src/app/pages/BuyPage/components/Tabbar/index.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import styled from 'styled-components';
-import { colors } from '../../../../constants';
+import classNames from 'classnames';
 import styles from './index.module.scss';
 interface ActiveProps {
   setSelectedTab: (e: boolean) => void;
   selectedTab: boolean;
 }
 
-const Bar = styled.div<{ active: boolean }>`
-  width: 146px;
-  height: 8px;
-  border-radius: 4px;
-  background: ${props => (!props.active ? 'transparent' : colors.main)};
-  @media (max-width: 600px) {
-    width: 106px;
-  }
-`;
-
 const Tabbar: React.FC<ActiveProps> = ({ setSelectedTab, selectedTab }) => {
   return (
     <div className={styles.tabContainer}>
       <div className={styles.barWrapper}>
         <p onClick={() => setSelectedTab(false)}>sovryn swap</p>
-        <Bar active={!selectedTab} />
+        <div
+          className={classNames(styles.bar, {
+            active: !selectedTab,
+          })}
+        ></div>
       </div>
       <div className={styles.barWrapper}>
         <p onClick={() => setSelectedTab(true)}>bonding curve</p>
-        <Bar active={selectedTab} />
+        <div
+          className={classNames(styles.bar, {
+            active: selectedTab,
+          })}
+        ></div>
       </div>
     </div>
   );
